78518c89c38be6aa1b73e56969333c0bee32781d
Dayana Puetate <68317594+Dayana-Puetate@users.noreply.github.com>
2024-02-29 11:54:44 -0500

    Titulo

--- src/gestionvc_ui/lib/main.dart
+++ src/gestionvc_ui/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nombre de tu Aplicación',
+      title: 'Gestion Ventas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
